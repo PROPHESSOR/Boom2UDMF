3bf274a1a3b9eb6a0db009547dd6372c061e6796
PROPHESSOR <xenteze@gmail.com>
2018-01-14 15:43:20 +0200

    Заготовка для дальнейшего заполнения экшн-карт

---
+++ config/actionConstants.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/* eslint quotes:["error", "double"] */
+
+
+module.exports = {
+	"Door": {
+		"Speed": {
+			"Slow": 32,
+			"Standart": 32,
+			"Fast": 32,
+			"Turbo": 32
+		},
+		"Key": {
+			"YellowCard": 0,
+			"YellowSkull": 0,
+			"RedCard": 0,
+			"RedSkull": 0,
+			"BlueCard": 0,
+			"BlueSkull": 0,
+			"AllCards": 0,
+			"AllSkulls": 0,
+			"AllKeys": 0
+		},
+		"Delay": { //
+			"Standart": 100
+		}
+	}
+};
--- config/lineActionMap.js
+++ config/lineActionMap.js
@@ -1,3 +1,15 @@
-'use strict';
+"use strict";
 
-module.exports = {};
+const Constant = require("./actionConstants");
+
+/* eslint quotes:["error", "double"] */
+
+module.exports = {
+	"1": {
+		"action": 12,
+		"arg1": 0, 		// Sector tag
+		"arg2": Constant.Door.Speed.Standart, 	// Speed
+		"arg3": Constant.Door.Delay.Standart, 	// Delay
+		"arg4": 0 		// Light tag
+	}
+};
