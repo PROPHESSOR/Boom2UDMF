45f8c8858901c9fefdf4bb306c290bc7a017b8d6
PROPHESSOR <xenteze@gmail.com>
2018-01-14 11:32:26 +0200

    Написан тест - пора действовать!

---
+++ test/test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const [
+	Linedef,
+	Sector,
+	Thing,
+	Sidedef,
+	Vertex
+] = [
+	require('../structures/linedef'),
+	require('../structures/sector'),
+	require('../structures/thing'),
+	require('../structures/sidedef'),
+	require('../structures/vertex')
+];
+
+console.log((new Vertex(0, 5, -5)).toString());
+console.log((new Linedef(0, 0, 1, 1, 0, 0, 0, 0)).toString());
+console.log((new Sidedef(0, 3, 3, 0, 0, 0, 0)).toString());
+console.log((new Thing(0, 3, 2, 90, 3, 7)).toString());
+console.log((new Sector(0, 0, 128, 1, 1, 128, 0, 0)).toString());
